96fe9a0ab98a17bf04073f4b40f2f149a803ab94
jeins <mail@mjuanakbar.info>
2015-07-07 12:07:53 +0200

    added basic structure

---
+++ src/resapi/Setup.php
@@ -0,0 +1,20 @@
+<?php
+ /*******************************************************
+ * Copyright (C) 2015 Muhammad Juan Akbar - All Rights Reserved
+ * Written by Muhammad Juan Akbar <mail@mjuanakbar.info>, 07 2015
+ *
+ * Setup.php can not be copied and/or distributed without the express
+ * permission of author.
+ *******************************************************/
+
+namespace resapi;
+
+use Slim\Slim;
+
+
+class Setup extends Slim{
+
+    public function __construct(){
+
+    }
+} 
