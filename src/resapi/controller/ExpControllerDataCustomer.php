4cee72500218e4a6f01f94d5d775956f8b46d468
Muhammad Juan Akbar <akbar@kieback-peter.de>
2015-07-08 15:52:40 +0200

    implement sample controller

--- src/resapi/controller/ExpControllerDataCustomer.php
+++ src/resapi/controller/ExpControllerDataCustomer.php
@@ -18,15 +18,75 @@ class ExpControllerDataCustomer extends Base{
         return new ExpModelDataCustomer();
     }
 
-    public function addNewCustomer(){}
+    public function addNewCustomer(){
+        try{
+            $db = $this->getDB();
 
-    public function changeCustomerData($id_customer){}
+            foreach($this->requestBodyArr as $key=>$value){
+                $db->$key = $value;
+            }
+            $db->save();
+            $this->writeToJSON($this->requestBodyArr, 201);
+        } catch (\Exception $e){
+            $this->writeToJSON(['errmsg' => 'service unavailable'], 503);
+        }
+    }
 
-    public function removeCustomer($id_customer){}
+    public function changeCustomerData($id_customer){
+        try {
+            $saveFile = $this->getDB()->query()->where('id_customer', '=', $id_customer)->update($this->requestBodyArr);
+            if($saveFile){
+                $this->writeToJSON($this->requestBodyArr);
+            } else{
+                $this->writeToJSON(['errmsg' => 'not found'], 204);
+            }
+        } catch (\Exception $e) {
+            $this->writeToJSON(['errmsg' => 'service unavailable'], 503);
+        }
+    }
 
-    public function getCustomers($id_customer = 0){echo "OK";}
+    public function removeCustomer($id_customer){
+        try{
+            $result = $this->getDB()->query()->where('id_customer', '=', $id_customer)->delete();
+            if($result){
+                $this->writeToJSON(['removed data with id' => $id_customer]);
+            } else{
+                $this->writeToJSON(['errmsg' => 'not found'], 204);
+            }
+        } catch (\Exception $e) {
+            $this->writeToJSON(['errmsg' => 'service unavailable'], 503);
+        }
+    }
 
-    public function filterCustomerBy(){}
+    public function getCustomers($id_customer = 0, $orderBy = "id_customer", $orderType = "asc"){
+        try {
+            if($id_customer == 0){
+                $this->writeToJSON($this->getDB()->orderBy($orderBy, $orderType)->get()->toArray());
+            } else {
+                $this->writeToJSON($this->getDB()->query()->where('id_customer', '=', $id_customer)->get()->toArray());
+            }
+        } catch (\Exception $e) {
+            $this->writeToJSON(['errmsg' => 'service unavailable'], 503);
+        }
+    }
+
+    public function filterCustomerBy(){
+        try{
+            $url = parse_url($_SERVER['REQUEST_URI']);
+            parse_str($url['query'], $queryURL);
+
+            $query = $this->getDB()->query();
+
+            foreach($queryURL as $key=>$value){
+                $query->where($key, 'LIKE', "%$value%");
+            }
+
+            $results = $query->get()->toArray();
+            $this->writeToJSON($results);
+        } catch (\Exception $e){
+            $this->writeToJSON(['errmsg' => 'service unavailable'], 503);
+        }
+    }
 
     public function addCustomerItems(){}
 
